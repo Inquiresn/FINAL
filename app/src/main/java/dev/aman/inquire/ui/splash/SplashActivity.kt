59bf8861bb6665651ea72a1782116e0ecffb82b0
codecraft26 <codescraft26@gmail.com>
2022-02-26 15:39:59 +0530

    Splash screen setup

--- app/src/main/java/dev/aman/inquire/ui/splash/SplashActivity.kt
+++ app/src/main/java/dev/aman/inquire/ui/splash/SplashActivity.kt
@@ -1,12 +1,20 @@
 package dev.aman.inquire.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import dev.aman.inquire.MainActivity
 import dev.aman.inquire.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val mIntent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }, 2000)
     }
 }
